ebf981ceb22444daa30ea45a43f95087d0bf0489
mathildechauvet <mathildechauvet@outlook.fr>
2026-02-23 19:30:25 +0100

    début de création d'un admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,14 +2,20 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
+        $admin = new User();
+        $admin->setRoles(['ROLE_ADMIN']);
         // $manager->persist($product);
 
         $manager->flush();
